71655cccb303910d51c2a33dc1b6c2d9b5ee4902
lucasrosati <lucasrosati@hotmail.com>
2023-10-13 19:16:40 -0300

    URL's e Paths dos apps no Django adicionados.

--- projeto_donare/app_criar_perfil_ong/views.py
+++ projeto_donare/app_criar_perfil_ong/views.py
@@ -1,3 +1,9 @@
+# Create your views here.
+
 from django.shortcuts import render
 
-# Create your views here.
+def criar_perfil_ong(request):
+    return render(request, 'cadastro_ong/perfil_ong.html')
+
+def dados_bancarios_ong(request):
+    return render(request, 'cadastro_ong/dados_bancarios.html')
