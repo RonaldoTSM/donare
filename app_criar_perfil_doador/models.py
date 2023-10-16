07dbed5e5540239cf8667bb4bb698335c3b30b8b
Arthurffreiree <ArthurDuarteFreire@gmail.com>
2023-10-16 11:58:11 -0300

    implementação banco de dados 1

--- app_criar_perfil_doador/models.py
+++ app_criar_perfil_doador/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=50)
+    usuario = models.CharField(max_length=100, unique=True)
+    senha = models.CharField(max_length=50)  # Recomenda-se armazenar senhas de forma segura (hashing e salting).
+    cpf = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+    telefone = models.CharField(max_length=11)
+
+    # Adicione outros campos conforme necessário
+    # Exemplo: data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.usuario
