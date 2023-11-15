c0fae304168283867ce0b885b330970177513511
joaonovaes1 <joaomnovaes1998@gmail.com>
2023-11-15 12:17:22 -0300

    finalizando os testes de criacao de perfil do doador

--- e2e_tests/test_criacao_perfil_doador.py
+++ e2e_tests/test_criacao_perfil_doador.py
@@ -14,3 +14,14 @@ class CriacaoTest(unittest.TestCase):
     def test_perfil_doador(self):
         
         self.browser.get("https://donare.azurewebsites.net/cadastro_doador/perfil_doador/")
+        
+        nome_input = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(By.ID, "nome_de_usuario"))
+        nome_completo = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(By.ID, "nome_completo"))
+        cpf = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(By.ID, "CPF"))
+        data_nascimento = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(By.ID, "Nascimento"))
+        descricao_textarea = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="cadastroForm"]/div/textarea')))
+        campo_data = self.browser.find_element_by_id("Nascimento")
+        campo_cep = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="cadastroForm"]/div/input[5]')))
+        campo_rua = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(By.XPATH, '//*[@id="cadastroForm"]/div/input[6]'))
+        
+        # <input type="text" placeholder="Rua" required="">
