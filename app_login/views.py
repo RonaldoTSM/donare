23c831975966de0b73ffedab7c5ab52bba6774a4
RonaldoTSM <ronaldotsm@outlook.com>
2023-11-25 15:24:32 -0300

    criação das home pages

--- app_login/views.py
+++ app_login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from app_criar_perfil_ong.models import CadastroOng
 from app_criar_perfil_doador.models import Cadastro
+from django.urls import reverse
 
 def login(request):
     if request.method == 'POST':
@@ -9,14 +10,14 @@ def login(request):
         try:
             user_ong = CadastroOng.objects.get(username=username)
             if user_ong.senha == senha:
-                return redirect('home_ong')  
+                return redirect(reverse('home_ong', kwargs={'username': username}))  
             else:
                 return render(request, 'login/login.html', {'error': 'Senha incorreta.'})
         except CadastroOng.DoesNotExist:
             try:
                 user_doador = Cadastro.objects.get(username=username)
                 if user_doador.senha == senha:
-                    return redirect('home_doador')  
+                    return redirect(reverse('home_doador', kwargs={'username': username}))  
                 else:
                     return render(request, 'login/login.html', {'error': 'Senha incorreta.'})
             except Cadastro.DoesNotExist:
--- app_pagina_inicial/views.py
+++ app_pagina_inicial/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from app_criar_perfil_ong.models import CadastroOng
+from app_criar_perfil_doador.models import Cadastro
 
-def home_doador(request):
-    return render(request, 'paginas/home_doador.html')
+def home_ong(request, username):
+    ong = get_object_or_404(CadastroOng, username=username)
+    return render(request, 'paginas/home_ong.html', {'ong': ong})
 
-def home_ong(request):
-    return render(request, 'paginas/home_ong.html')
+def home_doador(request, username):
+    doador = Cadastro.objects.get(username=username)
+    return render(request, 'paginas/home_doador.html', {'doador': doador})
